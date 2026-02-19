ff8e83b8676627b88e521d0945e2136e730d2640
leera131095-spec <leera131095@gmail.com>
2026-02-19 20:24:47 +0530

    Update student-shell.tsx

--- components/student-shell.tsx
+++ components/student-shell.tsx
@@ -1,4 +1,71 @@
+"use client"
+
+import { useState } from "react"
+
 export function StudentShell(){
+
+const [page,setPage]=useState("dashboard")
+
+if(page==="books"){
+
+return(
+
+<div className="fairytale-bg">
+
+<div className="fairytale-card">
+
+<h1>📚 Available Books</h1>
+
+<p>Cinderella</p>
+
+<p>Harry Potter</p>
+
+<p>Little Women</p>
+
+<button className="magic-button"
+onClick={()=>setPage("dashboard")}
+>
+Back
+</button>
+
+</div>
+
+</div>
+
+)
+
+}
+
+
+if(page==="issued"){
+
+return(
+
+<div className="fairytale-bg">
+
+<div className="fairytale-card">
+
+<h1>⭐ My Books</h1>
+
+<p>Harry Potter</p>
+
+<p>Fine: ₹10</p>
+
+<button
+className="magic-button"
+onClick={()=>setPage("dashboard")}
+>
+Back
+</button>
+
+</div>
+
+</div>
+
+)
+
+}
+
 
 return(
 
@@ -8,24 +75,18 @@ return(
 
 <h1>🌸 Student Library</h1>
 
-<p>Welcome to your magical reading space</p>
-
-<button className="magic-button">
-
+<button
+className="magic-button"
+onClick={()=>setPage("books")}
+>
 📖 View Books
-
 </button>
 
-<button className="magic-button">
-
-⭐ My Issued Books
-
-</button>
-
-<button className="magic-button">
-
-🔎 Search Library
-
+<button
+className="magic-button"
+onClick={()=>setPage("issued")}
+>
+⭐ My Books
 </button>
 
 </div>
