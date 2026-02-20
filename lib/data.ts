c6a6fcd4d2d7d30255705a958d56fec3c9b7c03e
leera131095-spec <leera131095@gmail.com>
2026-02-20 19:29:10 +0530

    Create data.ts

---
+++ lib/data.ts
@@ -0,0 +1,33 @@
+export const students = [
+  {
+    id: "ST001",
+    name: "Leena Rahman",
+    password: "1234",
+  },
+  {
+    id: "ST002",
+    name: "Aisha",
+    password: "1234",
+  },
+]
+
+export const books = [
+  {
+    id: "B001",
+    title: "Harry Potter",
+    author: "J.K Rowling",
+  },
+  {
+    id: "B002",
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+  },
+]
+
+export const issuedBooks = [
+  {
+    studentId: "ST001",
+    bookTitle: "Harry Potter",
+    issueDate: "2026-02-01",
+  },
+]
